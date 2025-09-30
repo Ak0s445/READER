40a918bab111b495849b0ca2d1ba93a2c357aea9
Ak0s445 <cipolakacsoakos5@gmail.com>
2025-09-30 09:03:47 +0200

    Fájlbeolvasás kész

--- src/Reader.java
+++ src/Reader.java
@@ -1,3 +1,6 @@
 public class Reader {
-    
+    public void startReader(){
+        Readfile rf = new Readfile(); //példányositjuk a readfilet hogy tudjunk vele dolgozni
+        rf.readContent();
+    }
 }
--- src/Readfile.java
+++ src/Readfile.java
@@ -1,3 +1,34 @@
-public class Readfile {
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+public class Readfile implements Readable{
+
+    //itt létrehozzuk az adatbeolvasást
+
+    @Override
+    public void readContent(){
+        try{
+            tryReadContent();
+        }catch(IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void tryReadContent()  throws IOException{
+      File file = new File("adat.txt");
+      Scanner sc = new Scanner(file, Charset.forName("utf-8"));
+      StringBuffer sb = new StringBuffer();
+        while (sc.hasNext()){
+            sb.append(sc.nextLine());
+
+        }
+        String content = sb.toString();
+        System.out.println("Tartalom: ");
+        System.out.println(content);
+        sc.close();
+    }
     
 }
